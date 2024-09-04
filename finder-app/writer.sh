77a72ba6220e53725155cf4a08017ba7e10fed40
abth5346 <113575727+abth5346@users.noreply.github.com>
2024-09-04 16:33:44 -0500

    added old files

---
+++ finder-app/writer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#By Abel Thomas
+
+writefile=$1
+writestr=$2
+
+#check for two arguments
+if [ $# -eq 2 ] 
+then
+	#check that dir exists
+	if [ ! -d "${writefile}" ]
+	then
+	
+		#if not create file	
+		mkdir -p "$(dirname $writefile)"
+	fi
+	
+	#put in contents
+	echo "${writestr}" > "${writefile}"
+	exit 0
+else 
+	echo "Two arguments required <writefile> <writestr>"	
+	exit 1	
+fi
